e8d119ae14ad81e194002a5f87f412aa1c09460c
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2019-02-20 12:50:09 +0100

    fix: post excerpt in HTML
    `gatsby-transformer-remark` puts some spaces with plain text excerpt.

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export const pageQuery = graphql`
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
-          excerpt
+          excerpt(format: HTML)
           fields {
             slug
           }
