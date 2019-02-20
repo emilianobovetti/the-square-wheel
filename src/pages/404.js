0f7e33f52a4042096e19ed5dbdda9d073e3f1ee0
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2019-02-20 13:43:48 +0100

    style: 404 gif resize

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,31 +3,39 @@ import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import rhythm from '../utils/typography/rhythm';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const GiphyResponsiveWrapper = styled.div`
-  width:100%;
-  height:0;
-  padding-bottom:57%;
-  position:relative;
+  position: relative;
+  padding-bottom: 56.8%;
+  margin-top: ${rhythm(5 / 2)};
+  height: 0;
+
+  @media screen and (min-width: 568px) {
+  }
+
+  @media screen and (min-width: 768px) {
+  }
 `;
 
 const GiphyIframe = styled.iframe`
-  position:absolute;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `;
 
 const NotFoundPage = ({ data }) =>
   <Layout title={data.site.siteMetadata.title}>
     <SEO lang="en" title="404: Not Found" />
-    <h1>404 Not Found</h1>
+    <h1>404: Not Found</h1>
     <GiphyResponsiveWrapper>
       <GiphyIframe
         src="https://giphy.com/embed/joV1k1sNOT5xC"
-        width="100%"
-        height="100%"
         frameBorder="0"
-        class="giphy-embed"
         allowFullScreen>
       </GiphyIframe>
     </GiphyResponsiveWrapper>
