4eef0be1244429e2adfe7874bdb9073d8701199b
Emiliano Bovetti <emiliano.bovetti@prima.it>
2022-12-06 19:39:36 +0100

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,20 @@
 set -e
 
 if [ -z "$NETLIFY_AUTH_TOKEN" ]; then
-    echo "NETLIFY_AUTH_TOKEN not setted"
-    exit 1
+  echo "NETLIFY_AUTH_TOKEN not setted"
+  exit 1
 fi
 
 if [ -z "$NETLIFY_SITE_ID" ]; then
-    echo "NETLIFY_SITE_ID not setted"
-    exit 1
+  echo "NETLIFY_SITE_ID not setted"
+  exit 1
 fi
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    npx netlify deploy \
-        --message="$TRAVIS_BRANCH@$(git rev-parse --short HEAD)" \
-        --auth="$NETLIFY_AUTH_TOKEN" \
-        --site="$NETLIFY_SITE_ID" \
-        --dir=public \
-        --prod
+if [ "$DRONE_BRANCH" = "master" ]; then
+  "$DRONE_WORKSPACE/node_modules/.bin/netlify" deploy \
+    --message="$DRONE_BRANCH@$(git rev-parse --short HEAD)" \
+    --auth="$NETLIFY_AUTH_TOKEN" \
+    --site="$NETLIFY_SITE_ID" \
+    --dir=public \
+    --prod
 fi
